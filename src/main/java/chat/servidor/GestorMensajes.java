b41f55c4a4fb5c216a7338eb3388b0046abb2a7b
Mafer <mafer_rj@ciencias.unam.mx>
2025-07-09 22:54:20 -0600

    Se agregaron mas métodos a el gestor de mensajes

--- src/main/java/chat/servidor/GestorMensajes.java
+++ src/main/java/chat/servidor/GestorMensajes.java
@@ -2,6 +2,9 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import java.util.HashMap;
 import java.util.Map;
+import com.google.gson.Gson;
+import java.io.*;
+import java.util.*;
 
 
 public class MessageManager {
@@ -22,42 +25,42 @@ public class MessageManager {
 
 	    switch () {
 	    case "IDENTIFY" :
-		identifyUser(client, message);
+		identifyUser(client, message); //identificar usuarios
 		break;
 	    case "STATUS" :
-		changeStatus(client, message);
+		changeStatus(client, message); //cambiar estado
 		break;
 	    case "USERS" :
-		sendUserList(client);
+		sendUserList(client);//enviar lista usuarios
 	        break;	
 	    case "TEXT" :
-		sendPrivateText(client, message);
+		sendPrivateText(client, message); //enviar texto privado
 		break;
 	    case "PUBLIC_TEXT" :
-		sendPublicText(client, message);
+		sendPublicText(client, message);//enviasr texto publico
 		break;
 	    case "NEW_ROOM" :
-		createRoom(client, message);
+		createRoom(client, message);//crear sala
 		break;
 	    case "INVITE" :
-		inviteUsers(client, message);
+		inviteUsers(client, message);//invitar usuarios
 	    case "JOIN_ROOM" :
-		joinARoom(client, message);
+		joinARoom(client, message);//unirse a sala
 		break;
 	    case "ROOM_USERS" :
-		sendUserListToTheRoom(client, message);
+		sendUserListToTheRoom(client, message);//enviar_lista_usuarios_sala
 		break;
 	    case "ROOM_TEXT" :
-		sendTextToTheRoom(client, message);
+		sendTextToTheRoom(client, message);//enviar_texto_sala
 		break;
 	    case "LEAVE_ROOM" :
-		leaveRoom(client, message);
+		leaveRoom(client, message);//dejar_sala
 		break;
 	    case "DISCONNECT" :
-		disconnectUser(client);
+		disconnectUser(client);//desconectar_usuario
 		break;
 	    default:
-		sendInvalidResponse(client);
+		sendInvalidResponse(client);//enviar_respuesta_invalida
 	    }	    
 	} catch (IOException e) {
 	   sendInvalidResponse(client); 
@@ -66,8 +69,8 @@ public class MessageManager {
 
     public void identifyUser(Client client, JSONObject message) {
 	throws IOException {
-	    String username = mensaje.optString("username");
-	    JSONObject answer = new JSONObject();
+	    String username = message.optString("username");
+	    Map<String, Object> answer = new HashMap<>();
 
 	    if (users.containsKey(username)) {
 		//Usuario ya existe
@@ -82,20 +85,68 @@ public class MessageManager {
 
 		answer.put("type","RESPONSE");
 		answer.put("operation","IDENTIFY");
-		answer.put("result","USER_ALREADY_EXISTS");
+		answer.put("result","SUCCESS");
 		answer.put("extra",username);
 
-		notifyNewUser(username);
+		notifyNewUser(username); //notificar_nuevo_usuario
 	    }
-	    //Enviar respuesta al cliente
-	    client.send(respuesta.toString());
+	    Gson gson = new Gson();
+	    String json = gson.toJson(answer);
+	    
+	    PrintWriterbout = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), "UTF-8"), true);
+	    out.println(json);
 	}
     }
+    
     public void changeStatus(Client client, JSONObject message) {
-	
+	throws IOException {
+	    String username = this.getUserByClient(client); //obtener_usuario_por_cliente
+	    if (username != null && !username.isEmpty()) {
+		String newStatus = message.optString("status");
+		users.get(username).put("status", newStatus) ;
+		notifyNewStatus(username, newStatus);
+	    }
+	}
     }
-    public void sendUserList() {}
-    public void sendPrivateText() {}
+    
+    public void sendUserList(Cliente cliente) {
+	Map<String, String> userList = new HashMap<>();
+	for(Map<String, User> entry : users.entrySet()) {
+	    userList.put(entry.getKey(), entry.getValue().getStatus());
+	    //Crear respuesta
+	    Map<String, Object>  answer = new HashMap<>();
+	    answer.put("type","USER_LIST");
+	    answer.put("users",userList);
+
+	    //Convertir a JSON y enviar
+	    Gson gson = new Gson();
+	    String json = gson.toJson(respuesta);
+
+	    try {
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), "UTF-8"),true );
+		out.println(json);
+ 	    } catch(IOException e) {
+		e.printStackTrace();	
+	    }  
+	}
+    }
+    public void sendPrivateText(Cliente cliente, JSONObject message ) {
+	String recipient = message.optString("username");
+	String text = message.optString("text");
+	if (users.containsKey(recipient)) {
+	    Cliente clientDestination = this.usuarios(recipient, "client");
+	    Map<String, Object>  answer = new HashMap<>();
+	    answer.put("type", "TEXT_FROM");
+	    answer.put("username", this.getUserByClient(client));
+	    answer.put("text", texto);
+	    clientDestination.
+	} else {
+	    answer.put("type", "RESPONSE");
+	    answer.put("operation", "TEXT");
+	    answer.put("result", "NO_SUCH_USER");
+	    answer.put("extra", recipient);
+	}
+    }
     public void sendPublicText() {}
     public void createRoom() {}
     public void inviteUsers() {}
